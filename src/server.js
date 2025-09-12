4d1aae2cd2b94d49e07ed8a8f61baae8d40a6c20
IamPiyush03 <piyushdhondge03@gmail.com>
2025-09-12 23:28:13 +0530

    feat(config): add PATCH method to CORS policy

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
     'https://goal-platform-client-t6ix-7veln9xfi-piyushs-projects-815384e6.vercel.app'
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
